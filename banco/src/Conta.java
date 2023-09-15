19f38b601549b1b64ed8c592e229449262c0e791
biangomes <beanascigom@gmail.com>
2023-09-15 13:21:48 -0300

    removing

--- banco/src/Conta.java
+++ banco/src/Conta.java
@@ -6,11 +6,18 @@ public class Conta {
     double saldo;
 
     void depositar(double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor do depósito deve ser maior que zero");
+        }
         saldo += valor;
     }
 
     void sacar(double valor) {
-        if ((saldo - valor) < 0) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor do saque deve ser maior que zero.");
+        }
+
+        if (saldo - valor < 0) {
             throw new IllegalStateException("Saldo insuficiente.");
         }
 
