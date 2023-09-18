57ffb55ca2d7d6451df9081da1ca47fd8bd362d7
beatriz_gomes <beatriz.gomes@softplan.com.br>
2023-09-18 14:10:39 -0300

    Usando BigDecimal ao inves de double

--- banco/src/app/TesteCalculo.java
+++ banco/src/app/TesteCalculo.java
@@ -1,12 +1,14 @@
 package app;
 
+import java.math.BigDecimal;
+
 public class TesteCalculo {
     public static void main(String[] args) {
 
-        double x = 1.0;
-        x = x - 0.10;
-        x = x - 0.20;
-        x = x - 0.30;
+        BigDecimal x = new BigDecimal("1.00");
+        x = x.subtract(new BigDecimal("0.10"));
+        x = x.subtract(new BigDecimal("0.20"));
+        x = x.subtract(new BigDecimal("0.30"));
         System.out.println(x);
     }
 }
